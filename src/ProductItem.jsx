0c73eaa9e32082b0aefaa8342d881f7f5eccc61d
MarsMwau <marthamumbua16@gmail.com>
2023-05-05 15:36:08 +0300

    Added the necessary buttons needed

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -25,6 +25,32 @@ const ProductItem = ({ product, resetClickedProduct }) => {
     setCartItems(newCartItems);
     setTotalPrice(totalPrice - item.price);
   };
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-6">
+          <img src={image} alt={title} height="400px" width="400px" />
+        </div>
+        <div className="col-md-6">
+          <h4>{category}</h4>
+          <h1>{title}</h1>
+          <h3 className="display-6 fw-bold my-4">${price}</h3>
+          <p className="lead-fw-bolder">
+            Rating {rating && rating.rate}
+            <i className="fa fa-star"></i>{" "}
+          </p>
+          <p className="lead">{description}</p>
+          <button className="btn btn-outline-dark px-4 py-2" onClick={addToCart}>
+            Add to Cart
+          </button>
+          <button className="btn btn-dark ms-2 px-3 py-2" onClick={resetClickedProduct}>
+                           Back to Products
+           </button>
+        </div>
+      </div>
+      <Cart cartItems={cartItems} totalPrice={totalPrice} onRemoveFromCart={onRemoveFromCart} />
+    </>
+  );
 
 };
 export default ProductItem;
